0c4b123e7ebd559ec7dcffcb3e9e97f1fdc32c6e
James Declerus <james.declerus@esih.edu>
2019-04-08 16:18:11 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
+
 def using_until
         levitation_force = 6
         #your code here
+        
+until levitation_force == 10
+  puts "The current number is less than 10."
+  counter += 1
+end
     
 end
 
